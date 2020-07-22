36c5a5e35f49903e96ac20eaa603c8fe925ae2e4
Ezra Tsai <ishow8166@icloud.com>
2020-07-22 16:16:48 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const Todo = require('./models/todo') // 載入 Todo model
+const bodyParser = require('body-parser')
 
 const app = express()
 
-mongoose.connect('mongodb://localhost/todo-list',{ useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
 // 取得資料庫連線狀態
 const db = mongoose.connection
 // 連線異常
@@ -16,11 +18,27 @@ db.once('open', () => {
     console.log('mongodb connected!')
 })
 
-app.engine('hbs', exphbs({ defaultLayout: "main", extname: '.hbs'}))
+app.engine('hbs', exphbs({ defaultLayout: "main", extname: '.hbs' }))
 app.set('view engine', 'hbs')
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-    res.render('index')
+    Todo.find() // 取出 Todo model 裡的所有資料
+        .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+        .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
+        .catch(error => console.loge(error)) // 錯誤處理
+})
+
+app.get('/todos/new', (req, res) => {
+    return res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+    const name = req.body.name         // 從 req.body 拿出表單裡的 name 資料
+    return Todo.create({ name })       // 存入資料庫
+        .then(() => res.redirect('/')) // 新增完成後導回首頁
+        .catch(error => console.log(error))
 })
 
 app.listen(3000, () => {

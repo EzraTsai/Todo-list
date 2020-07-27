d877491c66aaefc9939e413f1cdeaaaf39bd1592
Ezra Tsai <ishow8166@icloud.com>
2020-07-27 13:59:26 +0800

    refactor: modigy mongoose config

--- app.js
+++ app.js
@@ -1,22 +1,11 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
-const Todo = require('./models/todo') // 載入 Todo model
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override') // 載入method-override
-const app = express()
+const routes = require('./routes') // 引用路由器
+require('./config/mongoose')
 
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-    console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-    console.log('mongodb connected!')
-})
+const app = express()
 
 app.engine('hbs', exphbs({ defaultLayout: "main", extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -24,62 +13,8 @@ app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(methodOverride('_method'))
-
-app.get('/', (req, res) => {
-    Todo.find() // 取出 Todo model 裡的所有資料
-        .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-        .sort({ _id: 'asc' }) //排列正序asd 逆序desc
-        .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
-        .catch(error => console.loge(error)) // 錯誤處理
-})
-
-app.get('/todos/new', (req, res) => {
-    return res.render('new')
-})
-
-app.post('/todos', (req, res) => {
-    const name = req.body.name         // 從 req.body 拿出表單裡的 name 資料
-    return Todo.create({ name })       // 存入資料庫
-        .then(() => res.redirect('/')) // 新增完成後導回首頁
-        .catch(error => console.log(error))
-})
-
-app.get('/todos/:id', (req, res) => {
-    const id = req.params.id
-    return Todo.findById(id)
-        .lean()
-        .then((todo) => res.render('detail', { todo }))
-        .catch(error => console.log(error))
-})
-
-app.get('/todos/:id/edit', (req, res) => {
-    const id = req.params.id
-    return Todo.findById(id)
-        .lean()
-        .then((todo) => res.render('edit', { todo }))
-        .catch(error => console.log(error))
-})
-
-app.put('/todos/:id', (req, res) => {
-    const id = req.params.id
-    const { name, isDone } = req.body
-    return Todo.findById(id)
-    .then(todo => {
-        todo.name = name
-        todo.isDone = isDone === 'on'
-        return todo.save()
-    })
-    .then(() => res.redirect(`/todos/${id}`))
-    .catch(error => console.log(error))
-})
-
-app.delete('/todos/:id', (req, res) => {
-    const id = req.params.id
-    return Todo.findById(id)
-    .then(todo => todo.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
+// 將 request 導入路由器
+app.use(routes)
 
 app.listen(3000, () => {
     console.log('App is running on localhost: 3000')

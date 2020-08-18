cc207fa25f86d01c817f1871248a6223dbebce25
Ezra Tsai <ishow8166@icloud.com>
2020-08-18 23:58:02 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override') // 載入method-override
+const flash = require('connect-flash')
 const routes = require('./routes') // 引用路由器
 const usePassport = require('./config/passport') // 載入設定檔，要寫在 express-session 以後
 require('./config/mongoose')
@@ -24,11 +25,12 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 // 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
 usePassport(app)
-
+app.use(flash())
 app.use((req, res, next) => {
-    // console.log(req.user)
     res.locals.isAuthenticated = req.isAuthenticated()
     res.locals.user = req.user
+    res.locals.success_msg = req.flash('success_msg') // 設定 success_msg 訊息
+    res.locals.warning_msg = req.flash('warning_msg') // 設定 warning_msg 訊息
     next()
 })
 
